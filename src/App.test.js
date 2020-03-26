134f1855f625155d77180143e8bfd6f6e1a6c5c3
Charles Oliver <charles@cloudThing.com>
2020-03-26 19:42:01 +0000

    write tests for isAvailable function

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,70 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { format } from "date-fns/fp";
+import { pipe, propOr, contains } from "ramda";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+const isAvailable = availability => ({ date, time }) => {
+  const dateKey = format("dd/mm/yyyy", date);
+
+  return pipe(
+    propOr([], dateKey),
+    // make the assumption that all time slots are 1hr long for now
+    contains({ startTime: time }),
+  )(availability);
+};
+
+describe("isAvailable", () => {
+  const available = [
+    {
+      date: "26/03/2020",
+      availableSlots: [
+        {
+          startTime: "9:00",
+          endTime: "10:00",
+        },
+        {
+          startTime: "10:00",
+          endTime: "11:00",
+        },
+      ],
+    },
+    {
+      date: "27/03/2020",
+      availableSlots: [
+        {
+          startTime: "15:00",
+          endTime: "16:00",
+        },
+        {
+          startTime: "16:00",
+          endTime: "17:00",
+        },
+      ],
+    },
+  ];
+
+  it("returns true, when the timeslot is available", () => {
+    const fn = isAvailable(available);
+    const input = [
+      { date: new Date(2020, 2, 26), time: "9:00" },
+      { date: new Date(2020, 2, 26), time: "09:00" },
+      { date: new Date(2020, 2, 26), time: "10:00" },
+      { date: new Date(2020, 2, 27), time: "15:00" },
+      { date: new Date(2020, 2, 27), time: "16:00" },
+    ];
+
+    input.forEach(i => expect(fn(i)).toBe(true));
+  });
+
+  it("returns false, when the timeslot is unavailable", () => {
+    const fn = isAvailable(available);
+    const input = [
+      { date: new Date(2020, 2, 26), time: "11:00" },
+      { date: new Date(2020, 2, 26), time: "18:00" },
+      { date: new Date(2020, 2, 21), time: "10:00" },
+    ];
+
+    input.forEach(i => expect(fn(i)).toBe(false));
+  });
 });
