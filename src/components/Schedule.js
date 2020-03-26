021e70fcd566d000d91282d89a999508400f4197
Charles Oliver <charles@cloudThing.com>
2020-03-26 18:17:40 +0000

    begin styling components

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { addDays, format } from "date-fns/fp";
 import { flip, range, pipe } from "ramda";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
 const DAYS_IN_WEEK = 7;
 const MINUTES_IN_HOUR = 60;
@@ -11,13 +13,7 @@ const END_OF_SCHEDULE = 20 + 1;
 
 const padTime = str => str.toString().padStart(2, "0");
 
-const getTimezone = () => {
-  const gmtOffsetHours = -5 || new Date().getTimezoneOffset() / MINUTES_IN_HOUR;
-  const [hours, partHours = 0] = gmtOffsetHours.toString().split(".");
-  const sign = hours < 0 ? "-" : "+";
-
-  return `${sign}${padTime(Math.abs(hours))}:${partHours ? "30" : "00"}`;
-};
+const getTimezone = () => format("xxxxx", new Date());
 
 export const Schedule = ({
   currentDate,
@@ -28,14 +24,38 @@ export const Schedule = ({
   const hours = range(START_OF_SCHEDULE, END_OF_SCHEDULE).map(padTime);
 
   return (
-    <div>
-      <div>Your timezone: GMT {getTimezone()}</div>
-      <div>
+    <div
+      css={css`
+        align-items: center;
+        color: #374149;
+        display: flex;
+        flex-direction: column;
+        font-size: 1.4rem;
+        font-weight: 700;
+      `}
+    >
+      <div
+        css={css`
+          margin: 24px;
+        `}
+      >
+        Your timezone: GMT {getTimezone()}
+      </div>
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
         {days.map((d, i) => (
           <Day key={i} date={d} />
         ))}
       </div>
-      <div>
+      <div
+        css={css`
+          display: flex;
+          margin-top: 16px;
+        `}
+      >
         {hours.map(h => (
           <Time key={h} hour={h} />
         ))}
@@ -45,10 +65,46 @@ export const Schedule = ({
 };
 
 const Day = ({ date }) => (
-  <div>
-    <div>{format("do", date)}</div>
-    <div>{format("E", date)}</div>
+  <div
+    css={css`
+      align-items: center;
+      background: #f8f9fa;
+      border-radius: 8px;
+      /* border: 3px solid #5b58f3; */
+      display: flex;
+      flex-direction: column;
+      margin: 8px;
+      padding: 12px;
+      width: 120px;
+    `}
+  >
+    <div
+      css={css`
+        padding: 4px;
+      `}
+    >
+      {format("do", date)}
+    </div>
+    <div
+      css={css`
+        padding: 4px;
+      `}
+    >
+      {format("E", date)}
+    </div>
   </div>
 );
 
-const Time = ({ hour }) => <div>{hour}:00</div>;
+const Time = ({ hour }) => (
+  <div
+    css={css`
+      padding: 8px 16px;
+      border: 2px solid black;
+      border-radius: 8px;
+      margin: 8px;
+      font-size: 1.5rem;
+    `}
+  >
+    {hour}:00
+  </div>
+);
